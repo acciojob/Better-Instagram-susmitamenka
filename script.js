edd85284cc087a4b1d2e028d9e26808cff9c6759
susmitamenka <susmitaprasad73@gmail.com>
2025-02-09 16:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+let d = document.querySelectorAll(".image")
+div.addEventListener("dragstart",(e)=>{
+	console.log("Drag start");
+	e.dataTransfer.setData("text",div.id)
+})
+div.addEventListener("dragend",(e) =>{
+	console.log("Drag end")
+})
+let arr =["dragover","dragenter","drop"]
+arr.forEach(items =>{
+	let cols = document.querySelectorAll('.image')
+	for(let t of cols{
+		t.addEventListener(items,(e)=>{
+			e.preventDefault();
+			if(items == "drop"){
+			let imageId = e.dataTransfer.getData("image");
+			let movingImage = document.getElementById(imageId);
+			// t.append(movingImage)
+			}
+		})
+	}
+	})
+})
